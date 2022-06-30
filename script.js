605167bf0f8b7dfa609ea4a6098fdee3ebf12073
Steve Vazquez <stevevazquez0624@gmail.com>
2022-06-30 10:15:29 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Login button
+
+function login(element) {
+    console.log(element);
+    element.innerText = "Logout";
+}
+
+// Remove Definition when clicked
+
+function removeElement(element) {
+    console.log("remove the Add Definition when is clicked");
+    element.remove();
+}
